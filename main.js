52d65779e4564266f686a95c3e8edef659e566b4
Namr2789 <83170416+Namr2789@users.noreply.github.com>
2022-01-02 15:58:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+webcam.attach( '#camera' );
+
+function takeSnapshot(){
+ webcam.snap(function(data_uri){
+     document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+});
+}
+
+console.log('ml5version:',ml5version);
+
+classifier = ml5.imageclassifier
